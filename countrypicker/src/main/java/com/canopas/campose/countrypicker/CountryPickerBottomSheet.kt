398fd81f137ccf5004ee7e86ee187be6524b4ca9
Radhika canopas <74301808+cp-radhika-s@users.noreply.github.com>
2022-12-29 12:41:19 +0530

    Remove unnecessary log (#8)

--- countrypicker/src/main/java/com/canopas/campose/countrypicker/CountryPickerBottomSheet.kt
+++ countrypicker/src/main/java/com/canopas/campose/countrypicker/CountryPickerBottomSheet.kt
@@ -1,6 +1,5 @@
 package com.canopas.campose.countrypicker
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,18 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.Text
-import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -51,8 +40,6 @@ fun CountryPickerBottomSheet(
     }
 
     LaunchedEffect(key1 = modalBottomSheetState.currentValue) {
-        Log.e("State updates", "Current " + modalBottomSheetState.currentValue)
-
         if (modalBottomSheetState.currentValue == ModalBottomSheetValue.Hidden) {
             onDismissRequest()
         }
