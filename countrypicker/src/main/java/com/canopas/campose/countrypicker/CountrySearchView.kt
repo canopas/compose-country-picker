662211f241fd0139a262874b8b854476c4e210b6
Radhika Canopas <radhika.s@canopas.com>
2023-08-03 12:17:20 +0530

    Minor change

--- countrypicker/src/main/java/com/canopas/campose/countrypicker/CountrySearchView.kt
+++ countrypicker/src/main/java/com/canopas/campose/countrypicker/CountrySearchView.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.canopas.jetcountrypicker.R
 
 @Composable
 fun CountrySearchView(searchValue: String, onSearch: (searchValue: String) -> Unit) {
